017dc332b942946181b9afabf44f988166e53d24
Mustafaallam20 <mustafaallam2016@gmail.com>
2021-06-05 20:08:45 -0700

    initializing files

---
+++ Header.h
@@ -0,0 +1,5 @@
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+#include "Math.h"
+
+#define PI 3.14159265358979323846
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "Header.h"
+
+
+
+int main()
+{
+	while(1)
+	{
+			
+	}
+}

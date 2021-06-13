250b6ff3082a5f63abd464a058a90feafd9cfb13
Menna1808382 <85137499+Menna1808382@users.noreply.github.com>
2021-06-13 20:22:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -149,3 +149,41 @@ void delay(int time){
   delay(5);
   
 }
+
+void UART2_Init(void){ 
+	
+	
+    SYSCTL_RCGCUART_R |=0x0004; 
+    SYSCTL_RCGCGPIO_R |=0x0008; 
+
+   UART2_CTL_R &= ~0x0001; 
+   UART2_IBRD_R = 104; 
+   UART2_FBRD_R = 11; 
+   UART2_LCRH_R = 0x0070; 
+   UART2_CTL_R |= 0x0301; 
+
+    GPIO_PORTD_LOCK_R = 0x4C4F434B;
+    GPIO_PORTD_CR_R |= 0xC0;
+    GPIO_PORTD_AMSEL_R &= ~0xC0;
+    GPIO_PORTD_PCTL_R &= ~0x0FF000000;
+    GPIO_PORTD_DIR_R |= 0x80; 
+    GPIO_PORTD_DIR_R &= ~0x40;
+    GPIO_PORTD_AFSEL_R |= 0xC0;
+    GPIO_PORTD_DEN_R |= 0xC0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+uint8_t UART2_Available(void){ 
+return ((UART2_FR_R&0x0010) == 0x0010) ?  0 : 1; 
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+uint8_t UART2_Read(void){
+  while(UART2_Available() != 1); 
+  return (uint8_t)(UART2_DR_R&0xFF); 
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void UART2_Write(uint8_t data){
+  while((UART2_FR_R&0x0020) != 0); 
+  UART2_DR_R = data; 
+}
+
+

db4736c7c156960799b32c3a46eb592fdcf4c0df
Menna1808382 <85137499+Menna1808382@users.noreply.github.com>
2021-06-13 20:35:48 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -13,3 +13,8 @@ void ledOn(double d);
 double distance(double lati1, double long1, double lati2, double long2);
 void delay(int time);
 void seven_segment(int one, int two, int three);
+char UART2_read();
+void UART2_Init();
+uint8_t UART2_Available(void);
+uint8_t UART2_Read(void);
+void UART2_Write(uint8_t data);
